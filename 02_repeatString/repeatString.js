914c07a677edf8b41a09e8e17a77070fb5badf7b
iRpro16 <rumeoisidoro16@gmail.com>
2024-08-22 21:44:27 -0400

    exercises 01 to 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let phrase = "";
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            phrase += string;
+        }
+    } else {
+        return "ERROR";
+    }
+    return phrase;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // set empty array
+    let arr = [];
+    // spread operator to turn iterable to array
+    arr = [...string];
+    // reverse order of array
+    let reverseString = arr.reverse();
+    // join array with empty string and then convert to string
+    let reversedString = reverseString.join("").toString()
+    // return string
+    return reversedString;
 };
 
 // Do not edit below this line
