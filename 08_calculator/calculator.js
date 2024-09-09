29626e0e366ab765d03023e85bcaeb5cfa5b9ece
iRpro16 <rumeoisidoro16@gmail.com>
2024-09-09 19:23:31 -0400

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let initialValue = 0;
+  return array.reduce((acc, curr) => acc + curr, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let array = [];
+  let initialValue = 1;
+    for (i = 1; i <= num; i++) {
+        array.push(i);
+      }
+      return array.reduce((acc, curr) => acc * curr, initialValue);
 };
 
 // Do not edit below this line
