dcd6b9c542f805a15a07b1af0c7b80eb1fee975e
iRpro16 <rumeoisidoro16@gmail.com>
2024-09-09 19:36:12 -0400

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphanumeric = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const cleanString = string
+    .toLowerCase()
+    .split("")
+    .filter((e) => alphanumeric.includes(e))
+    .join("")
 
+    const reverseString = cleanString.split("").reverse().join("");
+
+    return cleanString === reverseString;
 };
 
 // Do not edit below this line
