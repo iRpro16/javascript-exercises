09474a65fcbee676738ea8e459f35b55a78932f3
iRpro16 <rumeoisidoro16@gmail.com>
2024-09-09 20:29:14 -0400

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    const array  = [0,1];
+    for (i = 0; i < num; i++) {
+        let lastInt = array[array.length - 1];
+        let secondLastInt = array[array.length -2];
+        array.push(lastInt + secondLastInt);
+    }
+    if (num >= 0) {
+        return array[array.length - 2];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
